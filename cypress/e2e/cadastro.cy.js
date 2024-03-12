1d265eaaf40601fa5079d6dfbabece420f3fa295
Arthur Leal <arthurleal1@hotmail.com>
2024-03-12 14:33:47 -0300

    Adicao dos testes de cadastro

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,4 +1,4 @@
-import CadastroPage from '../support/pages/cadastro.page';
+import CadastroPage from "../page-objects/cadastro-page.cy"
 
 describe('Testes na Funcionalidade Cadastro', ()=>{
     beforeEach(()=>{
@@ -14,37 +14,46 @@ describe('Testes na Funcionalidade Cadastro', ()=>{
         cadastroPage.clicarBotaoRegister( )
         //esperar que a mensagem de erro seja exibida
         cy.get('.woocommerce-error').should('be.visible')
-        cy.get('.woocommerce-error').should('have.text', '\n\t\t\tError: Username is required.\n\t')
+        cy.get('.woocommerce-error').should('have.text', '\n\t\t\tError: Please provide a valid email address.\n\t')
     })
 
     it('Realizar cadastro com email vazio e senha preenchida', ()=>{
         const cadastroPage = new CadastroPage();
 
-        cadastroPage.inserirPassword('Arthur_Leal980')
+        cadastroPage.inserirPassword('Arthur^Leal980')
+        cadastroPage.inserirPassword('Arthur^Leal980')
+        cadastroPage.inserirPassword('Arthur^Leal980')
+
         cadastroPage.clicarBotaoRegister( )
         //esperar que a mensagem de erro seja exibida
         cy.get('.woocommerce-error').should('be.visible')
-        cy.get('.woocommerce-error').should('have.text', '\n\t\t\tError: Username is required.\n\t')
+        cy.get('.woocommerce-error').should('have.text', '\n\t\t\tError: Please provide a valid email address.\n\t')
     })
 
     it('Realizar cadastro com email preenchido e senha vazia', ()=>{
         const cadastroPage = new CadastroPage();
 
-        cadastroPage.inserirEmail('arthur2')
+        //inserir um email com valor arthur+numero aleatorio+@arthur.com
+        let numeroAleatorio = Math.floor(Math.random() * 1000)
+        cadastroPage.inserirEmail('arthur'+numeroAleatorio+'@arthur.com')
         cadastroPage.clicarBotaoRegister( )
         //esperar que a mensagem de erro seja exibida
         cy.get('.woocommerce-error').should('be.visible')
-        cy.get('.woocommerce-error').should('have.text', '\n\t\t\tError: Password is required.\n\t')
+        cy.get('.woocommerce-error').should('have.text', '\n\t\t\tError: Please enter an account password.\n\t')
     })
 
     it('Realizar cadastro com email e senha válidos', ()=>{
         const cadastroPage = new CadastroPage();
+        let numeroAleatorio = Math.floor(Math.random() * 1000)
 
-        cadastroPage.realizarCadastro('arthur2', 'Arthur_Leal980')
+        cadastroPage.realizarCadastro('arthur'+numeroAleatorio+'@arthur.com', 'Arthur^Leal980')
+        cadastroPage.inserirPassword('Arthur^Leal980')
+        cadastroPage.inserirPassword('Arthur^Leal980')
+
         //esperar que a mensagem de erro seja exibida
         cy.get('.woocommerce-MyAccount-content').should('be.visible')
         //preciso pegar o primeiro elemento p do .woocommerce-MyAccount-content e comparar o texto
-        cy.get('.woocommerce-MyAccount-content p').first().should('have.text', '\n\tHello arthur2 (not arthur2? Sign out)')
+        cy.get('.woocommerce-MyAccount-content p').first().should('have.text', '\n\tHello arthur'+numeroAleatorio+' (not arthur'+numeroAleatorio+'? Sign out)')
 
     })
 
--- cypress/page-objects/cadastro-page.cy.js
+++ cypress/page-objects/cadastro-page.cy.js
@@ -6,17 +6,21 @@ class CadastroPage{
 
     inserirEmail(email){
         cy.get(this.idEmail).should('be.visible')
+        cy.get(this.idEmail).clear()
+        cy.get(this.idEmail).click()
         cy.get(this.idEmail).type(email)
     }
 
     inserirPassword(password){
         cy.get(this.idPassword).should('be.visible')
+        cy.get(this.idPassword).clear()
+        cy.get(this.idPassword).click()
         cy.get(this.idPassword).type(password)
     }
 
     clicarBotaoRegister(){
-        cy.get('.register').should('be.visible')
-        cy.get('.register').click()
+        cy.get('input[name="register"]').should('be.visible')
+        cy.get('input[name="register"]').click()
     }
 
     realizarCadastro(email, password){
@@ -25,7 +29,6 @@ class CadastroPage{
         this.clicarBotaoRegister()
     }
 
-    
-
-
 }
+
+export default CadastroPage;
